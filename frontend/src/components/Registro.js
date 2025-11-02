3dfa68a29267cd971521434d7b94045e61557f04
Julian22077 <camilolozano0122@gmail.com>
2025-11-01 19:09:25 -0500

    Registro de usuarios, que los crea en base de datos mediante una peticion post de una ruta con express ademas un query

--- frontend/src/components/Registro.js
+++ frontend/src/components/Registro.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import './style.css'
 
 function Registro(){
 const [nombre,setNombre]=useState("");
@@ -34,29 +35,39 @@ const navigate=useNavigate();
     
      return(
        <div className="usuarios">
-            <label>Nombre:<input value={nombre}
-            onChange={(e)=>{
+        <div className="titulo">Registro</div>
+        <div class="form-floating mb-3">
+        <input  onChange={(e)=>{
                 setNombre(e.target.value)
-            }}
-            type="text"/></label>
-            <label>Email:<input value={email}
-            onChange={(e)=>{
+            }}type="text" class="form-control" id="floatingInput"/>
+        <label for="floatingInput">Nombre</label>
+        </div>
+         <div class="form-floating mb-3">
+        <input  onChange={(e)=>{
                 setEmail(e.target.value)
-            }}
-            type="text"/></label>
-            <label>Numero:<input value={telefono}
-            onChange={(e)=>{
+            }}type="emaill" class="form-control" id="floatingInput"/>
+        <label for="floatingInput">Email</label>
+        </div>
+         <div class="form-floating mb-3">
+        <input  onChange={(e)=>{
                 setNumero(e.target.value)
-            }}
-            type="text"/></label>
-            <label>Contraseña:<input value={contraseña}
-            onChange={(e)=>{
+            }}type="text" class="form-control" id="floatingInput"/>
+        <label for="floatingInput">Telefono</label>
+        </div>
+         <div class="form-floating mb-3">
+        <input  onChange={(e)=>{
                 setContraseña(e.target.value)
-            }}
-            type="password"/></label>
-            <button onClick={registrarse}>Registrarse</button>
-            <p>Ya estas registrado? inicia Sesion</p>
-            <button onClick={()=>navigate("/login")}>Iniciar Sesión</button>
+            }}type="text" class="form-control" id="floatingInput"/>
+        <label for="floatingInput">Contraseña</label>
+        </div>
+        <button onClick={registrarse} type="button" class="btn btn-outline-secondary">Registrarse</button>
+            <p>¿Ya estás registrado? Inicia Sesion</p>
+        <a onClick={()=>navigate("/login")} class="icon-link" href="#">
+  Iniciar Sesion
+  <svg xmlns="http://www.w3.org/2000/svg" class="bi" viewBox="0 0 16 16" aria-hidden="true">
+    <path d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+  </svg>
+</a>
         </div>
     );
 }
